25bf8372bff690798e0d9c6ce8dccce06ed0ef3e
juanlu-millan <juanlumillanhidalgo@gmail.com>
2019-02-28 18:52:42 +0100

    Fichero .py

--- ejercicio_xml.py
+++ ejercicio_xml.py
@@ -1,2 +1,7 @@
 from lxml import etree
 doc = etree.parse('colegios-lorca.xml')
+
+#Lista conctatos de cada colegio que no pertenece a la pedania de lorca
+def colegio(doc):
+    lista = doc.xpath("//Web/text() | //Email/text() | //Telefono/text() | //Fax/text()")
+    return lista
