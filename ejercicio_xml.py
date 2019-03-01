4b0a60e591a2b4d38ae83ade78d6763be98be98b
juanlu-millan <juanlumillanhidalgo@gmail.com>
2019-03-01 09:31:45 +0100

    Ejercicio 2

--- ejercicio_xml.py
+++ ejercicio_xml.py
@@ -12,8 +12,11 @@ def colegio(doc):
 for col in colegio(doc):
     print(col)
 
-Cuenta los colegios que no tiene página web y muestra su nombre
+#Cuenta los colegios que no tiene página web y muestra su nombre
 
 def colegio(doc):
     lista = doc.xpath("//Contacto[not(Web)]/../Centro/text()")
     return lista
+
+for col in colegio(doc):
+    print(col)
