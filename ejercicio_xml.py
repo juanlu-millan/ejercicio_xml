a6c4686745c75474c565328d73e1df0f84688faf
juanlu-millan <juanlumillanhidalgo@gmail.com>
2019-03-01 09:30:13 +0100

    Ejercicio 2 empezado

--- ejercicio_xml.py
+++ ejercicio_xml.py
@@ -3,5 +3,17 @@ doc = etree.parse('colegios-lorca.xml')
 
 #Lista conctatos de cada colegio que no pertenece a la pedania de lorca
 def colegio(doc):
-    lista = doc.xpath("//Web/text() | //Email/text() | //Telefono/text() | //Fax/text()")
+
+    if doc.xpath(//Localizacion[not(Pedania='Lorca')]/text()):
+        lista = doc.xpath("//Web/text() | //Email/text() | //Telefono/text() | //Fax/text() | //Localizacion[not(Pedania='Lorca')]")
+
+    return lista
+
+for col in colegio(doc):
+    print(col)
+
+Cuenta los colegios que no tiene página web y muestra su nombre
+
+def colegio(doc):
+    lista = doc.xpath("//Contacto[not(Web)]/../Centro/text()")
     return lista
