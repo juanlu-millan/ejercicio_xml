934fac678db290e864782ebf7241f4433d222130
juanlu-millan <juanlumillanhidalgo@gmail.com>
2019-03-02 15:14:36 +0100

    Ultimos pasos

--- ejercicio_xml.py
+++ ejercicio_xml.py
@@ -4,28 +4,34 @@ doc = etree.parse('colegios-lorca.xml')
 while (True):
     print('''
     Elige una opcion:
-    1. Número total de postales
-    2. Número total de postales por Estado
-    3. URL de ciudad
+    1. Lista conctatos de cada colegio que no pertenece a la pedania de lorca
+    2. Cuenta los colegios que no tiene página web y muestra su nombre
+    3. Pide una pedania y muestra el nombre de los colegios y su dirección
+    4. Introduce un número de teléfono y muestra el Centro al que pertenece
+    5. URL de Centro
     0-Salir''')
     opcion=int(input("Opcion: "))
 
     if opcion==1:
 
-        #Lista conctatos de cada colegio que no pertenece a la pedania de lorca
-        # def colegio(doc):
-        #
-        #     if doc.xpath(//Localizacion[not(Pedania='Lorca')]/text()):
-        #         lista = doc.xpath("//Web/text() | //Email/text() | //Telefono/text() | //Fax/text() | //Localizacion[not(Pedania='Lorca')]")
-        #
-        #     return lista
-        #
+    #Lista conctatos de cada colegio que no pertenece a la pedania de lorca
+        def colegio(doc):
+            lista = doc.xpath('''//Localizacion[not(Pedania='Lorca')]/../Centro/text() |
+            //Localizacion[not(Pedania='Lorca')]/../Contacto/Web/text() |
+            //Localizacion[not(Pedania='Lorca')]/../Contacto/Telefono/text() |
+            //Localizacion[not(Pedania='Lorca')]/../Contacto/Email/text()
+            //Localizacion[not(Pedania='Lorca')]/../Contacto/Fax/text()
+            ''')
+
+            return lista
+
         for col in colegio(doc):
             print(col)
 
+
     elif opcion==2:
 
-        #Cuenta los colegios que no tiene página web y muestra su nombre
+    #Cuenta los colegios que no tiene página web y muestra su nombre
 
         def colegio(doc):
             lista = doc.xpath("//Contacto[not(Web)]/../Centro/text()")
@@ -37,7 +43,7 @@ while (True):
 
     elif opcion==3:
 
-        #Pide una pedania y muestra el nombre de los colegios y su dirección
+    #Pide una pedania y muestra el nombre de los colegios y su dirección
 
         def poblacion(doc):
             pueblo=input("Dime una pedania:").capitalize()
@@ -49,19 +55,33 @@ while (True):
             print (centros," - ",calle)
         print ("")
 
-    #
-    # elif opcion==4:
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    # elif opcion==5:
+    #Introduce un número de teléfono y muestra el Centro al que pertenece
+
+    elif opcion==4:
+        def telefono(telefono,doc):
+            centro =  doc.xpath('')
+            return centro
+        telefono=input("Dime número de telefono de un centro:")
+
+        print ()
+
+    #Introduce nombre del Instituto y te da un enlace a OpenStreetMap de su localización
+
+    elif opcion==5:
+        def localizar(doc):
+            centro=input("Dime el nombre del centro para obtener su cordenada:").upper()
+            latitud = str(doc.xpath('//colegio_lorca[Centro="%s"]/Localizacion/Coordenadas/Latitud/text()'%centro))
+            longitud = str(doc.xpath('//colegio_lorca[Centro="%s"]/Localizacion/Coordenadas/Longitud/text()'%centro))
+
+            latitud2 = latitud.replace("[","").replace("]","").replace("'","")
+            longitud2 = longitud.replace("[","").replace("]","").replace("'","")
+            print (latitud2,latitud2)
+            localizador="http://www.openstreetmap.org/#map=16/%s/%s"%(latitud2,longitud2)
+            return localizador
+
+        print (localizar(doc))
+
+
 
     elif opcion == 0:
         break;
